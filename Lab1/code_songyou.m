5b1652ddc192d2ea382cd16796fe732b3f1379e0
pengsongyou <psy920710@gmail.com>
2015-11-24 15:21:39 +0100

    Add what we dont understand

--- Lab1/code_songyou.m
+++ Lab1/code_songyou.m
@@ -4,6 +4,10 @@ im_0 = imread('/Users/Songyou/Documents/VIBOT/UB/Sensors and Digitization/Sensor
 im_45 = imread('/Users/Songyou/Documents/VIBOT/UB/Sensors and Digitization/Sensors_Lab_Git/Lab1/Pictures/wolff/45.jpg');
 im_90 = imread('/Users/Songyou/Documents/VIBOT/UB/Sensors and Digitization/Sensors_Lab_Git/Lab1/Pictures/wolff/90.jpg');
 im_no = imread('/Users/Songyou/Documents/VIBOT/UB/Sensors and Digitization/Sensors_Lab_Git/Lab1/Pictures/wolff/no.jpg');
+hsv = rgb2hsv(im_no);
+aa = hsv(:,:,3);
+imagesc(hsv);
+%%
 %change pictures to gray
 im_0 = rgb2gray(im_0);
 im_45 = rgb2gray(im_45);
@@ -18,22 +22,23 @@ subplot(121); imshow(I); title('total light intensity');
 subplot(122); imshow(im_no); title('without polarization');
  
 %compute the phi pictures
-im_phi = (I - 2.* im_45)./(I - 2.*im_90); 
-phi = atan(im_phi)*180/3.1415/2;
+% im_phi = (I - 2.* im_45)./(I - 2.*im_90); 
+phi = atan2(I - 2.* im_45, I - 2.*im_90)*180/3.1415/2;
 figure();
  % ?? it is weird here. two image's are similar.
 %  subplot(121);imshow(im_phi);
 %  subplot(122);imshow(phi);
-imshow(phi)
+imagesc(phi);% Angle of Polarization
 
 %compute the degree of polarization
 DOP = sqrt((I - 2.*im_45).^2 + (I - 2.*im_90).^2)./I;
 figure();
-imshow(DOP); title('Degree of Polarization');
+imagesc(DOP); title('Degree of Polarization');
 
 imwrite(I,'/Users/Songyou/Documents/VIBOT/UB/Sensors and Digitization/Sensors_Lab_Git/Lab1/Pictures/wolff/I.jpg');
 imwrite(DOP,'/Users/Songyou/Documents/VIBOT/UB/Sensors and Digitization/Sensors_Lab_Git/Lab1/Pictures/wolff/DOP.jpg'); 
-imwrite(phi,'/Users/Songyou/Documents/VIBOT/UB/Sensors and Digitization/Sensors_Lab_Git/Lab1/Pictures/wolff/AOP.jpg');
+%imwrite(phi,'/Users/Songyou/Documents/VIBOT/UB/Sensors and Digitization/Sensors_Lab_Git/Lab1/Pictures/wolff/AOP.jpg');
+
 %% Least Mean Square method
 %read the image first
 N = 9;
@@ -87,13 +92,13 @@ imwrite(S2,'/Users/Songyou/Documents/VIBOT/UB/Sensors and Digitization/Sensors_L
 
 I_Least = S0;
 DOP_Least = sqrt(S1.*S1 + S2.*S2) ./ S0;
-Phi_Least = atan(S2./S1)*180/3.1415/2;
-imshow(Phi_Least);title('Degree of Polarization');
+Phi_Least = atan2(S2, S1)*180/3.1415/2;
+imshow(DOP_Least);title('Degree of Polarization');
 figure();
-imshow(Phi_Least);title('Angle of Polarization');
+imagesc(Phi_Least);
 imwrite(I_Least,'/Users/Songyou/Documents/VIBOT/UB/Sensors and Digitization/Sensors_Lab_Git/Lab1/Pictures/Least_Mean/I_Least.jpg');
 imwrite(DOP_Least,'/Users/Songyou/Documents/VIBOT/UB/Sensors and Digitization/Sensors_Lab_Git/Lab1/Pictures/Least_Mean/DOP_Least.jpg')
-imwrite(Phi_Least,'/Users/Songyou/Documents/VIBOT/UB/Sensors and Digitization/Sensors_Lab_Git/Lab1/Pictures/Least_Mean/Phi_Least.jpg');
+%imwrite(Phi_Least,'/Users/Songyou/Documents/VIBOT/UB/Sensors and Digitization/Sensors_Lab_Git/Lab1/Pictures/Least_Mean/Phi_Least.jpg');
 
 %%
 Combine = zeros(height, width, 3);
@@ -102,10 +107,27 @@ Combine(:,:,2) = DOP_Least;
 Combine(:,:,3) = Phi_Least;
 imshow(Combine)
 
-%% sinusoidal
-b = [im{1}(20,20)];
-for k = 2 : N
-    b = [b,im{k}(20,20)];
+%% sinusoidal of different position
+point_in_screen = [];
+point_in_phone = [];
+point_in_other = [];
+for k = 1 : N
+    point_in_screen = [point_in_screen,im{k}(200,100)];%point in the screen
+    point_in_phone = [point_in_phone,im{k}(500,150)];%point in the phone
+    point_in_other = [point_in_other,im{k}(300,300)];%point in other place
 end
-plot(b,'r');
+plot(point_in_screen,'r');
+hold on;
+plot(point_in_phone, 'g');
+plot(point_in_other, 'b');
+hold off;
 
+%% sinusoidal of s0, s1, s2
+point_in_s0 = [];
+point_in_s1 = [];
+point_in_s2 = [];
+for k = 1 : N
+    point_in_s0 = [point_in_s0,im{k}(200,100)];%point in the screen
+    point_in_s1 = [point_in_s1,im{k}(500,150)];%point in the phone
+    point_in_s2 = [point_in_s2,im{k}(300,300)];%point in other place
+end
